bc50426cee4beaa996477ce43d148a9552c16ada
pravalik12 <pspreddy121@gmail.com>
2023-04-11 12:08:50 +0530

    first commit

---
+++ src/app/base-component/base-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-component',
+  templateUrl: './base-component.component.html',
+  styleUrls: ['./base-component.component.css']
+})
+export class BaseComponentComponent {
+
+}
